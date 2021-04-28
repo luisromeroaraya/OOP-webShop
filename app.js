edb299bcc0d8e8318422401ba6df40f97279ef7d
Marcelo Romero-Cors <luisromeroaraya@gmail.com>
2021-04-28 12:14:25 +0200

    Change sql createConnect to sql createPool

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 // https://www.youtube.com/watch?v=A01KtJTv1oc
+// https://www.youtube.com/watch?v=1aXZQcG2Y6I
+
+// IMPORT ENV CONFIG FILE
+require('dotenv').config();
+
 // IMPORT EXPRESS
 const express = require("express");
 const app = express();
+
+// IMPORT EXPRESS HANDBLEBARS
+const exphbs = require("express-handlebars");
 
 // IMPORT ROUTER
 const router = require("./routers/router");
@@ -9,9 +17,11 @@ const router = require("./routers/router");
 // SET UP PORT
 const port = process.env.PORT || 3000; // port will use the environment PORT if is set or else 3000
 
-// STATIC FILES
-app.use(express.urlencoded({ extended: true}));
+// BODY PARSER
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+// STATIC FILES
 app.use(express.static("public"));
 app.use("/css", express.static(__dirname + "public/css"));
 app.use("/js", express.static(__dirname + "public/js"));
@@ -22,24 +32,25 @@ app.use("/webfonts", express.static(__dirname + "public/webfonts"));
 // CONNECT TO ROUTER
 app.use("/", router);
 
-// DB
+// CREATE CONNECTION POOL TO DB
 const mysql = require("mysql");
-const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "freshshop"
+const pool = mysql.createPool({
+    connectionLimit: 100,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
 // CONNECT TO DB
-db.connect((err) => {
-    if(err){
+pool.getConnection((err, connection) => {
+    if (err) {
         throw err;
     }
-    console.log("Database connected...");
+    console.log(`Connected to database as ID: ${connection.threadId}`);
 });
 
 // SET UP LISTENER (SERVER)
-app.listen(port, function() {
+app.listen(port, () => {
     console.info(`Listening on port ${port}...`);
 });
